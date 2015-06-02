00bbe8d10b64caa893107bd08822eb8ad64a5f35
wushian <sam@young-net.com.tw>
2015-06-02 21:18:36 +0800

    init

---
+++ KanbanBoardWithSignalRAngularJSSol/Controllers/BoardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using KanbanBoardWithSignalRAngularJSSol.Models;
+
+namespace KanbanBoardWithSignalRAngularJSSol.Controllers
+{
+    public class BoardController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }                      
+    }
+}
---
+++ KanbanBoardWithSignalRAngularJSSol/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(KanbanBoardWithSignalRAngularJSSol.Startup))]
+namespace KanbanBoardWithSignalRAngularJSSol
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {            
+            app.MapSignalR();
+        }
+    }
+}
